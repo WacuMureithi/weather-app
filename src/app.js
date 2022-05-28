fedf39ed16159a576d95fcff4187b0143a4f1f4c
WacuMureithi <105235420+WacuMureithi@users.noreply.github.com>
2022-05-28 21:22:12 +0300

    Set forcecast data for 6 days

--- src/app.js
+++ src/app.js
@@ -23,36 +23,49 @@ function formatDate(timestamp) {
 
 }
 
+function formatDay(timestamp){
+
+  let date= new Date(timestamp *1000)
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+
+  return days[day];
+
+}
+
 function displayforecast(response){
 
-  console.log(response.data.daily);
+  let forecast = (response.data.daily);
+
   let forecastElement= document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
 
-  let forecastdays = ["Mon", "Tue", "Wed"];
-  forecastdays.forEach(function(day){
+ forecast.forEach(function(forecastDay, index){
+
+  if (index < 6) {
   forecastHTML =
+
     forecastHTML +
     `
         <div class="col-2">
             <div class="weather-forecast-day">
-                         ${day}
+                         ${formatDay(forecastDay.dt)}
             </div>
             
             <div class="weather-icon">
-                    <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width="70">
+                    <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="50">
             </div>
 
             <div class="weather-forecast-temperature">
-              <span id="weather-forecast-temperature-max">18°</span>
+              <span id="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
               |
-              <span class="weather-forecast-temperature-min">12°</span>
+              <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
               </div>
       </div>
   `;
 
-  })
+  }})
   ;
 
   forecastHTML = forecastHTML + `</div>`;
